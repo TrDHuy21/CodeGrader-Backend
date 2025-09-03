8dbd08c21171b23464c31b8318787755d7625ea1
TrDHuy21 <vlsvhuy@gmail.com>
2025-09-04 00:51:25 +0700

    doi ten docker-compsoe cap nhat run.sh them bookmark them env cho bookmark

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ echo "🔄 Updating CodeGrader Microservices..."
 
 # Pull latest images
 echo "📥 Pulling latest images..."
-docker-compose -f docker-compose.prod.yml pull
+docker-compose -f docker-compose.yml pull
 
 # Restart services with new images
 echo "🔄 Restarting services..."
-docker-compose -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.yml up -d
